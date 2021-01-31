1442d7ecd77048a9503f2bbad2eb6bf020999d7d
Md_Borhan_Chowdhury <borhanchowdhury192@gmail.com>
2021-01-31 21:11:41 +0600

    initial commit

---
+++ variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+
+
+var name string = "BORHAN"
+var name2, city string = "CHOWDHURY", "TUPAMARI"
+fmt.Println(name, name2, city)
+
+
+var c rune = 'A'
+var age int = 24
+var result float32 = 3.49
+
+fmt.Println(c, age, result)
+
+}
